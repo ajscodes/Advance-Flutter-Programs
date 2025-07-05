5d58da74564935945d779912d65824c1e8d694ec
ajscodes <ayushmaradia@gmail.com>
2025-07-05 17:06:59 +0530

    completed

--- lab_programs/lib/lab-1/a1_student_detail.dart
+++ lab_programs/lib/lab-1/a1_student_detail.dart
@@ -1,8 +1,20 @@
 class Student{
-  String? name;
+  late String name;
   late int age;
   late String email;
 
-  Student();
+  Student({required this.name,required this.age,required this.email});
 
+  void displayDetails(){
+    print('Student Details: ');
+    print('Name: $name');
+    print('Age: $age');
+    print('Email: $email');
+  }
+}
+
+void main(){
+  Student ayush = Student(name: 'Ayush', age: 20 , email: 'ayush@gmail.com');
+
+  ayush.displayDetails();
 }
